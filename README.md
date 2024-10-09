8f4b8c64b0d22cc6c6e3daece93267085a4b11bd
Nael Shiab <nael.shiab@gmail.com>
2024-10-09 11:00:33 -0400

    New version with local data and chart

--- README.md
+++ README.md
@@ -13,28 +13,32 @@ bunx --bun setup-sda
 deno run -A jsr:@nshiab/setup-sda
 ```
 
-There is one option:
+There are two options:
 
 - _--js_ : To use JavaScript instead of TypeScript.
+- _--force_ : To force the creation of files even if the folder is not
+  empty.
 
 The script will:
 
-- Make sure that the folder is empty.
+- Make sure that the folder is empty (unless you passed the option _--force_).
 
 - Check your runtime (Node.js, Bun, or Deno).
 
 - If you use Node.js:
 
-  - If it's >= 22.6.0, it will create an `main.ts` and `tsconfig.json` (unless
+  - If it's >= 22.6.x, it will create an `src/main.ts` and `tsconfig.json` (unless
     you passed the option _--js_)
-  - If it's < 22.6.0, it will create an `main.js`
+  - If it's < 22.6.x, it will create an `src/main.js`
 
 - If you use Bun or Deno, it will create an `index.ts` (unless you passed the
   option _--js_)
 
+- Create `data/temp.csv` with some data.
+
 - If you use Node.js or Bun, it will create a `package.json` with two scripts:
 
-  - `npm run sda` to run and watch `index.ts` or `index.js`
+  - `npm run sda` to run and watch `src/main.ts` or `src/main.js`
   - `npm run clean` to remove the folders `.sda-cache` (used by methods like
     [cache](https://nshiab.github.io/simple-data-analysis/classes/SimpleTable.html#cache))
     and `.temp` (used by [duckdb-node](https://github.com/duckdb/duckdb-node)
@@ -42,10 +46,10 @@ The script will:
 
 - If you use Deno, it will create a `deno.json` with two tasks:
 
-  - `deno task sda` to run and watch `index.ts`
+  - `deno task sda` to run and watch `src/main.ts`
   - `deno task clean` to remove the folders `.sda-cache` and `.temp`
 
-- Install the libraries (with NPM, Bun, or JSR):
+- Install the libraries (with NPM, Bun, or JSR, depending on the runtime):
 
   - [simple-data-analysis](https://github.com/nshiab/simple-data-analysis)
   - [journalism](https://github.com/nshiab/journalism)
--- simple-data-analysis.mjs
+++ simple-data-analysis.mjs
@@ -8,10 +8,16 @@ console.log("\nStarting sda setup...");
 
 const args = process.argv.slice(2);
 
-const files = readdirSync(".");
-if (files.length > 0) {
-  throw new Error(
-    "The folder is not empty. Please start over in an empty folder."
+if (!args.includes("--force")) {
+  const files = readdirSync(".");
+  if (files.length > 0) {
+    throw new Error(
+      "The folder is not empty. Please start over in an empty folder or pass the option --force."
+    );
+  }
+} else {
+  console.log(
+    "    => The folder is not empty but --force option is used. Overwriting files."
   );
 }
 
@@ -19,18 +25,20 @@ let mainContent = `import { SimpleDB } from "simple-data-analysis";
 import { prettyDuration } from "journalism";
 
 const start = Date.now();
-
 const sdb = new SimpleDB();
 
-const table = await sdb
-  .newTable()
-  .loadData(
-    "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/geodata/files/firesCanada2023.csv"
-  );
+const temp = sdb.newTable("temp");
+await temp.loadData("data/temp.csv");
 
-await table.logTable();
+await temp.logTable();
 
-await sdb.done()
+await temp.logDotChart("decade", "meanTemp", {
+  smallMultiples: "city",
+  fixedScales: true,
+  formatY: (d) => \`${d}°C\`,
+});
+
+await sdb.done();
 
 prettyDuration(start, { log: true, prefix: "\\nDone in " });
 `;
@@ -44,6 +52,64 @@ const tsconfigContent = `{
   "include": ["**/*"],
   "exclude": ["node_modules"]
 }
+`;
+const data = `city,decade,meanTemp
+Toronto,1840.0,7.1
+Toronto,1850.0,6.4
+Toronto,1860.0,6.9
+Toronto,1870.0,6.8
+Montreal,1880.0,5.3
+Toronto,1880.0,6.6
+Calgary,1890.0,2.7
+Montreal,1890.0,6.0
+Toronto,1890.0,7.6
+Calgary,1900.0,3.3
+Montreal,1900.0,5.8
+Toronto,1900.0,7.7
+Vancouver,1900.0,8.7
+Calgary,1910.0,3.5
+Montreal,1910.0,6.1
+Toronto,1910.0,8.1
+Vancouver,1910.0,8.8
+Calgary,1920.0,3.6
+Montreal,1920.0,6.2
+Toronto,1920.0,8.0
+Vancouver,1920.0,9.3
+Calgary,1930.0,4.1
+Montreal,1930.0,6.8
+Toronto,1930.0,8.6
+Vancouver,1930.0,9.6
+Calgary,1940.0,4.2
+Montreal,1940.0,6.9
+Toronto,1940.0,8.7
+Vancouver,1940.0,10.0
+Calgary,1950.0,3.2
+Montreal,1950.0,7.4
+Toronto,1950.0,9.2
+Vancouver,1950.0,9.8
+Calgary,1960.0,4.0
+Montreal,1960.0,7.4
+Toronto,1960.0,8.8
+Vancouver,1960.0,10.0
+Calgary,1970.0,3.8
+Montreal,1970.0,7.3
+Toronto,1970.0,9.0
+Vancouver,1970.0,9.6
+Calgary,1980.0,4.9
+Montreal,1980.0,7.7
+Toronto,1980.0,9.0
+Vancouver,1980.0,10.2
+Calgary,1990.0,4.5
+Toronto,1990.0,9.6
+Vancouver,1990.0,10.6
+Calgary,2000.0,4.7
+Montreal,2000.0,7.6
+Toronto,2000.0,9.7
+Vancouver,2000.0,10.6
+Calgary,2010.0,4.8
+Montreal,2010.0,8.0
+Toronto,2010.0,9.9
+Vancouver,2010.0,10.9
 `;
 
 console.log("\n1 - Checking runtime and options...");
@@ -168,6 +234,9 @@ if (language === "ts") {
   writeFileSync("main.js", mainContent);
   console.log("    => src/main.js has been created.");
 }
+mkdirSync("data");
+writeFileSync("data/temp.csv", data);
+console.log("    => data/temp.csv has been created.");
 
 if (runtime === "nodejs") {
   console.log("\n3 - Installing libraries with NPM...");
